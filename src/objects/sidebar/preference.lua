506ecc925e2819ad132a5605e5a63295a078372b
qwwshs <2559873038@qq.com>
2025-07-18 20:42:20 +0800

    Gpreference界面

--- src/objects/sidebar/preference.lua
+++ src/objects/sidebar/preference.lua
@@ -1,75 +1,31 @@
---子房间 就写成对象了
-local x = 0
-local y = 0
-local w = 0
-local h = 0
-local r = 0
-local input_type = false --输入状态
-local type = "preference"
-local preference_pos_y = 0 --位移
-local offset_x_ed = 0 --旧的偏移值
-local function will_draw()
-    return sidebar:room_type(type) and the_room_pos({"edit",'tracks_edit'})
-end
-local function input_ed() --记录旧的偏移
-    offset_x_ed = chart.preference.x_offset
-end
+--preference界面
+local Gpreference = group:new('preference')
+Gpreference.type = "preference"
+Gpreference.layout = require('config.layouts.sidebar').preference
+Gpreference.x_offset_v = {value = '0'}
 
-local function input_ed_finish() --应用偏移
-    if love.window.showMessageBox( "", i18n:get("Whether to offset the previously written event value"),{'no','yes'} ) == 2 then
-        for i = 1,#chart.event do
-            if chart.event[i].type == "x" then
-                chart.event[i].from = chart.event[i].from - chart.preference.x_offset + offset_x_ed
-                chart.event[i].to = chart.event[i].to - chart.preference.x_offset + offset_x_ed
+function Gpreference:load()
+    Gpreference.x_offset_v = {value = tostring(chart.preference.x_offset or 0)}
+end
+function Gpreference:Nui()
+    Nui:layoutRow('dynamic', self.layout.uiH, self.layout.cols)
+    Nui:edit('field',self.x_offset_v)
+
+    if Nui:button(i18n:get('save')) then
+        local old = chart.preference.x_offset
+        log(tonumber(self.x_offset_v.value))
+        chart.preference.x_offset = tonumber(self.x_offset_v.value) or 0
+
+        if love.window.showMessageBox( "", i18n:get("Whether to offset the previously written event value"),{'no','yes'} ) == 2 then
+            for i = 1,#chart.event do
+                if chart.event[i].type == "x" then
+                    chart.event[i].from = chart.event[i].from - chart.preference.x_offset + old
+                    chart.event[i].to = chart.event[i].to - chart.preference.x_offset + old
+                end
             end
         end
     end
 end
 
-object_preference = {
-    load = function(x1,y1,r1,w1,h1)
-        x= x1 --初始化
-        y = y1
-        w = w1
-        h = h1
-        r = r1
-        preference_pos_y = 0
-        if not sidebar:room_type(type) then
-            return
-        else
 
-            input_box_new("x_offset","chart.preference.x_offset",x,y+40 + preference_pos_y,w,20,{type ="string",will_draw = will_draw,input_ed = input_ed, input_ed_finish = input_ed_finish})
-        end
-
-        
-    end,
-    draw = function()
-        if not sidebar:room_type(type) then
-            return
-        end
-        
-
-        love.graphics.setColor(1,1,1,1)
-        love.graphics.print(i18n:get("x_offset"),x,y+20 + preference_pos_y) 
-    end,
-    mousepressed = function( x1, y1, button, istouch, presses )
-    end,
-    keypressed = function(key)
-        if not sidebar:room_type(type) then
-            return
-        end
-
-        
-    end,
-    wheelmoved = function(x,y)
-        if not sidebar:room_type(type) then
-            return
-        end
-        local scroll = 40
-        if y < 0 then
-            scroll = - scroll
-        end
-        input_box_wheelmoved(x,scroll,"x_offset")
-        preference_pos_y = preference_pos_y + scroll
-    end
-}
+return Gpreference
