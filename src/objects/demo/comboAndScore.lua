ff1201f8fa1368afb57f624af05b4b300bb686a0
qwwshs <2559873038@qq.com>
2026-05-02 20:05:35 +0800

    Performance Optimization

--- src/objects/demo/comboAndScore.lua
+++ src/objects/demo/comboAndScore.lua
@@ -8,17 +8,43 @@ for i = 0,11 do
     comboAndScore.numtab[i] = love.graphics.newImage("assets/img/font-combo-"..i..".png")
 end
 comboAndScore.numtab.w,comboAndScore.numtab.h = comboAndScore.numtab[0]:getDimensions()
-function comboAndScore:update(dt)
-    --计算
-    local combo = 0
-    local allTrueNote = 0
-    for i = 1,#chart.note do
-        local n = chart.note[i]
-        if n.fake == 0 then
+
+
+
+local combo = 0
+local allTrueNote = 0
+local previous_frame_beat = 0 -- 上一帧的节拍
+local previous_frame_starting_point = 1 -- 上一帧的遍历起点
+
+function comboAndScore:opened()
+    combo = 0
+    allTrueNote = 0
+    previous_frame_beat = 0
+    previous_frame_starting_point = 1
+
+    --算真note数量
+    for i, n in ipairs(chart.note) do
+        local _,w = fEvent:get(n.track,beat:get(n.beat))
+        if n.fake == 0 and w ~= 0 then
             allTrueNote = allTrueNote + 1
-            if beat:get(n.beat) <= beat.nowbeat and n.fake == 0 then
-                combo = combo + 1
-            end
+        end
+    end
+end
+
+function comboAndScore:update(dt)
+    if previous_frame_beat == beat.nowbeat then
+        return
+    end
+    previous_frame_beat = beat.nowbeat
+    for i = math.max(previous_frame_starting_point,1),#chart.note do
+        local n = chart.note[i]
+        local _,w = fEvent:get(n.track,beat:get(n.beat))
+        if beat:get(n.beat) < beat.nowbeat and n.fake == 0 and w ~= 0 then
+            combo = combo + 1
+        end
+        if beat:get(n.beat) > beat.nowbeat then
+            previous_frame_starting_point = i
+            break
         end
     end
     self.score = tostring(math.floor(1000000 * combo / allTrueNote))
@@ -33,19 +59,22 @@ function comboAndScore:draw()
     local scale = comboAndScore.scoreLayout.size* 1 / self.numtab.w
     local interval = comboAndScore.scoreLayout.size
     love.graphics.setColor(demo.colors.combo)
+    local v
+    local img
+    local x
     for i = 1,#self.combo do
-        local v = string.sub(self.combo,i,i)
+        v = string.sub(self.combo,i,i)
         if #v < 1 then break end
-        local img = self.numtab[tonumber(v)]
-        local x = self.comboLayout.x + (i - #self.combo / 2) * interval - comboAndScore.scoreLayout.size --love2d图片位置不是以中点算的
+        img = self.numtab[tonumber(v)]
+        x = self.comboLayout.x + (i - #self.combo / 2) * interval - comboAndScore.scoreLayout.size --love2d图片位置不是以中点算的
         love.graphics.draw(img,x,settings.judge_line_y + self.comboLayout.y,0,scale,scale)
     end
     love.graphics.setColor(demo.colors.score)
     for i = 1,#self.score do
-        local v = string.sub(self.score,i,i)
+        v = string.sub(self.score,i,i)
         if #v < 1 then break end
-        local img = self.numtab[tonumber(v)]
-        local x = self.scoreLayout.x + (i - #self.score / 2) * interval - comboAndScore.scoreLayout.size --love2d图片位置不是以中点算的
+        img = self.numtab[tonumber(v)]
+        x = self.scoreLayout.x + (i - #self.score / 2) * interval - comboAndScore.scoreLayout.size --love2d图片位置不是以中点算的
         love.graphics.draw(img,x,settings.judge_line_y + self.scoreLayout.y,0,scale,scale)
     end
 
--- src/objects/play/slider.lua
+++ src/objects/play/slider.lua
@@ -15,38 +15,6 @@ function slider:draw()
     love.graphics.setColor(editTool.colors.slider)
     love.graphics.rectangle('fill',self.x,self.y,self.w,self.h)
 
-    local local_tab = {1} --用来计算密度的
-    for i = 1 ,100 do
-        local_tab[i] = 1
-    end
-    --全谱平均note密度表示 分成100部分
-    for i = 1, #chart.note do
-        local pos = math.floor(beat:toTime(chart.bpm_list,beat:get(chart.note[i].beat)) / time.alltime * 100)
-        if not local_tab[pos] then --超界
-            local_tab[pos] = 1 
-        end
-
-        local_tab[pos] = local_tab[pos] + 1
-    end
-    --换算成相对高度
-    local max =local_tab[1]
-    local min =  local_tab[1]
-    for i = 1, 100 do
-        if max < local_tab[i] then
-            max = local_tab[i]
-        end
-        if local_tab[i] < min then
-            min = local_tab[i]
-        end
-    end
-    if max == min then max = min + 1 end
-    for i = 1, 100, 1 do
-        local max_h = self.w
-        local density_ratio = (local_tab[i]-min) / (max - min) --密度比例
-        love.graphics.setColor(density_ratio * 1,1 - density_ratio * 1,1 - density_ratio * 1,1)
-        love.graphics.rectangle("fill",self.x,self.y+ self.h - self.h / 100 * i,density_ratio * max_h,self.h / 100)
-    end
-
     love.graphics.setColor(editTool.colors.sliderLine)
     love.graphics.rectangle('line',self.x,self.y,self.w,self.h) -- 框
     love.graphics.setColor(editTool.colors.progress)
@@ -63,7 +31,7 @@ function slider:update(dt)
     local y1 = mouse.y
     if  y1 < self.y then y1 = self.y  end ---限制范围
     if y1 > self.y + self.h then  y1 = self.y + self.h  end
-    if slider.down  then
+    if slider.down then
         music_play = false
         self.now_y = y1
         time.nowtime = -(self.now_y-self.y - self.h) / self.h  * time.alltime
